584940b72e076ed9757268a875104d69b1f72364
nikocaetano <ellercaetano@gmail.com>
2022-09-14 23:22:29 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Componentes/Header";
+import Main from "./Componentes/Main"
+
+function APP(){
+  return(
+    <section>
+      <Header/>
+      <Main />      
+    </section>
+  )
+}
+
+export default APP
---
+++ src/Componentes/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <section>
+            <h1>Welcome to HP's World</h1>
+        </section>
+    )
+}
